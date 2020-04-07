0cd8829c07c38dddca80a93bad616d930686e9a2
Jeremy Jason <booya1402@gmail.com>
2020-04-07 15:36:37 +0700

    make input picture make corner radius

--- SoundBoardMini/ViewController.swift
+++ SoundBoardMini/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var onClick: UIButton!
+    @IBOutlet weak var savebutton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        savebutton.layer.cornerRadius = 20
+        img.layer.cornerRadius = 30
+        
     }
+    
+    @IBAction func onpickimage(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
 
+}
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+                
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+            img.image = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
