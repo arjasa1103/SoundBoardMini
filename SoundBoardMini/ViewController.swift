3eb5ec1de5acd354d1f448fba9c7bef24ac89b8f
Josia Mannuel <joy.mannuell@gmail.com>
2020-04-08 16:45:08 +0700

    add-tech-1 v2

--- SoundBoardMini/ViewController.swift
+++ SoundBoardMini/ViewController.swift
@@ -13,11 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var onClick: UIButton!
     @IBOutlet weak var savebutton: UIButton!
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var imgBox: UIImageView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        savebutton.layer.cornerRadius = 20
-        img.layer.cornerRadius = 30
+        savebutton.layer.cornerRadius = 16
+        img.layer.cornerRadius = 35
+        titleField.layer.cornerRadius = 18
         
     }
     
@@ -29,7 +34,15 @@ class ViewController: UIViewController {
         present(vc, animated: true)
     }
     
-
+    @IBAction func save(_ sender: Any) {
+        if titleField.text == "" {
+            let date = Date()
+            let formatter = DateFormatter()
+            formatter.dateFormat = "dd.MM.yyyy"
+            print(formatter.string(from: date))
+        }
+    }
+    
 }
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate
@@ -45,6 +58,22 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func shakeView(duration: CFTimeInterval, view: UIView) {
+        let shakeValues = [-5, 5, -5, 5, -3, 3, -2, 2, 0]
+
+        let translation = CAKeyframeAnimation(keyPath: "transform.translation.x");
+        translation.timingFunction = CAMediaTimingFunction(name: .linear)
+        translation.values = shakeValues
+        
+        let rotation = CAKeyframeAnimation(keyPath: "transform.rotation.z")
+        rotation.values = shakeValues.map { (Int(Double.pi) * $0) / 180 }
+        
+        let shakeGroup = CAAnimationGroup()
+        shakeGroup.animations = [translation, rotation]
+        shakeGroup.duration = 0.5
+        view.layer.add(shakeGroup, forKey: "shakeIt")
     }
     
 }
